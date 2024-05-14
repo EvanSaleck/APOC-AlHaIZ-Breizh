4e1a2ac8cfb9b2bc5a9696b702424d061e75e6d3
LucasGourmelon <lucasgourmelon12@gmail.com>
2024-05-14 17:26:51 +0200

    ADD index logements

---
+++ html/Views/Front/logement/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des logements</title>
+    <link rel="stylesheet" href="../../../assets/SCSS/logement.css">
+</head>
+<body>
+    <div id="headerAccueilMobile">
+        <img src="../../../assets/imgs/logo.png" id="logoAssoMobile" alt="Logo de l'association">
+        <button id="buttonfiltresMobile">
+            <img src="../../../assets/imgs/mobile/Menu.svg" alt="Logo menu">
+        </button>
+    </div>
+
+</body>
+</html>
