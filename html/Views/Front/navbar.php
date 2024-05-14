4295e64aa2000658eb902c81a57e4d517fcaadb9
rtroadec <ryan.troadec@etudiant.univ-rennes1.fr>
2024-05-14 15:04:06 +0200

    Composants IHM navbar

---
+++ html/Views/Front/navbar.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Navbar</title>
+        <link rel="stylesheet" type="text/css" href="/html/assets/SCSS/navbar.css">
+    </head>
+    <div>
+        <button>
+            <img src="/html/assets/imgs/mobile/Loupe.svg" alt=""> 
+            Explorer
+        </button>
+        <button>
+            <img src="/html/assets/imgs/mobile/MaisonArbre.svg" alt=""> 
+            Logements
+        </button>
+        <button>
+            <img src="/html/assets/imgs/mobile/User.svg" alt=""> 
+            Compte
+        </button>
+    </div>
+    <style>
+    </style>
+</html>
