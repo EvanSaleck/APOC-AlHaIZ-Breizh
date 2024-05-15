105158a5e98f356d019db98557ee4ee3faeeedc1
Lucas <lucasgourmelon12@gmail.com>
2024-05-15 21:55:18 +0200

    ADD database object

---
+++ html/Service/Database.php
@@ -0,0 +1,34 @@
+<?php
+class Database {
+    private $pdo;
+    private $dsn = 'pgsql:host=localhost;dbname=apoc;port=5432';
+    private $username = 'apoc';
+    private $password = 'apoc';
+
+    public function __construct() {
+        try {
+            $this->pdo = new PDO($this->dsn, $this->username, $this->password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            $this->pdo->exec("set schema 'sae3'");
+        } catch (PDOException $e) {
+            die('Erreur de connexion : ' . $e->getMessage());
+        }
+    }
+
+    public function getPDO() {
+        return $this->pdo;
+    }
+
+    public function executeQuery($query) {
+        try {
+            $statement = $this->pdo->prepare($query);
+            $statement->execute();
+            $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        } catch (PDOException $e) {
+            die('Erreur lors de l\'exécution de la requête : ' . $e->getMessage());
+        }
+    }
+}
+?>
