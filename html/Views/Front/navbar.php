41171f8abae729fbe69572f8693162b72a752dca
rtroadec <ryan.troadec@etudiant.univ-rennes1.fr>
2024-05-15 13:54:01 +0200

    fix navbar

--- html/Views/Front/navbar.php
+++ html/Views/Front/navbar.php
@@ -10,18 +10,18 @@
 </head>
 
 <body>
-    <div>
-        <button>
-            <img src="/html/assets/imgs/mobile/Loupe.svg" alt=""> 
-            Explorer
+    <div class="navbar">
+        <button class="bouttonExplorer">
+            <img class="imgExplorer" src="/html/assets/imgs/mobile/Loupe.svg" alt=""> 
+            <p class="nomBoutton">Explorer</p>
         </button>
-        <button>
-            <img src="/html/assets/imgs/mobile/MaisonArbre.svg" alt=""> 
-            Logements
+        <button class="bouttonLogements">
+            <img class="imgLogements" src="/html/assets/imgs/mobile/MaisonArbre.svg" alt=""> 
+            <p class="nomBoutton">Logements</p>
         </button>
-        <button>
-            <img src="/html/assets/imgs/mobile/User.svg" alt=""> 
-            Compte
+        <button class="bouttonCompte">
+            <img class="imgCompte" src="/html/assets/imgs/mobile/User.svg" alt=""> 
+            <p class="nomBoutton">Compte</p>
         </button>
     </div>
 </body>
