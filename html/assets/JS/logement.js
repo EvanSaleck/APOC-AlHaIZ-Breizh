e90ecd6051ff3d66d376f937524ac72314828611
Lucas <lucasgourmelon12@gmail.com>
2024-05-15 13:58:07 +0200

    ADD css et js de l'onglet de filtre

---
+++ html/assets/JS/logement.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('buttonfiltresMobile').onclick = function() {
+        document.getElementById('filtresMobile').classList.toggle('active');
+    }
+});
