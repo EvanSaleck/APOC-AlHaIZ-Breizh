0866bb3831b0d14abdadf14ede3ea028625d658d
LANSONNEUR EWAN <elansonneur@mx21>
2024-05-15 11:37:48 +0200

    ADD Premier commit de la branche, creation des fichiers associés à la page details d'un logement

---
+++ html/Views/Front/logement/détailsLogement.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Titre Logement - Ville</h1>
+    <img src="../../../assets/imgs/imageLogementPlaceholder.png" alt="Image du logement">
+    <div>
+        <p>Note</p>
+        <img id="etoileNotation" src="" alt="Etoile">
+        <img id="etoileNotation" src="" alt="Etoile">
+        <img id="etoileNotation" src="" alt="Etoile">
+        <img id="etoileNotation" src="" alt="Etoile">
+        <img id="etoileNotation" src="" alt="Etoile">
+    </div>
+</body>
+</html>
