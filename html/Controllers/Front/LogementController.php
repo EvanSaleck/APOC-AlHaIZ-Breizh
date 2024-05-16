d3656aa96b7876fea9a1addccfb15984f6e710aa
Lucas <lucasgourmelon12@gmail.com>
2024-05-16 10:59:01 +0200

    UPDATE ajout database

--- html/Controllers/Front/LogementController.php
+++ html/Controllers/Front/LogementController.php
@@ -2,12 +2,16 @@
 
 require 'Service/Database.php';
 
+use Service\Database;
+
 function indexAction()
 {
     $db = new Database();
     $logements = $db->executeQuery('SELECT * FROM logement');
 
     $logementsJson = json_encode($logements);
+
+    // $user = 'Lucas';
 
     require './Views/Front/logement/index.php';
 }
