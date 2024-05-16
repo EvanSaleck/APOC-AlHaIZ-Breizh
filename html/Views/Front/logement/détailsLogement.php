65a176c03b81b5ef2b88ebaa93bb37505e57e589
LANSONNEUR EWAN <elansonneur@mx21>
2024-05-16 15:40:37 +0200

    UPDATE : Completion du fichier détailsLogement.sass

--- html/Views/Front/logement/détailsLogement.php
+++ html/Views/Front/logement/détailsLogement.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="/assets/SCSS/détailsLogement.css" rel="stylesheet" type="text/css" />
+    <link href="/html/assets/SCSS/détailsLogement.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-    <main id=mainLogement>
+    <div id=descLogement>
         <section id=sectionDesc>
-            <img id=imageLogement src="/assets/imgs/imageLogementPlaceholder.png" alt="Image du logement">
+            <img id=imageLogement src="/html/assets/imgs/imageLogementPlaceholder.png" alt="Image du logement">
             <div id=divTitre>
                 <h1 id=titreLog>Maison de campagne</h1>
                 <h1> - </h1>
@@ -17,18 +17,22 @@
             </div>
             <article id=artPrixNote>
                 <div id="note">
-                    <img id="etoileNotation" src="/assets/imgs/notes/star_full.svg" alt="Etoile">
-                    <img id="etoileNotation" src="/assets/imgs/notes/star_full.svg" alt="Etoile">
-                    <img id="etoileNotation" src="/assets/imgs/notes/star_full.svg" alt="Etoile">
-                    <img id="etoileNotation" src="/assets/imgs/notes/star_full.svg" alt="Etoile">
-                    <img id="etoileNotation" src="/assets/imgs/notes/star_full.svg" alt="Etoile">
+                    <p>Note</p>
+                    <img id="etoileNotation1" src="/html/assets/imgs/notes/star_full.svg" alt="Etoile">
+                    <img id="etoileNotation2" src="/html/assets/imgs/notes/star_full.svg" alt="Etoile">
+                    <img id="etoileNotation3" src="/html/assets/imgs/notes/star_full.svg" alt="Etoile">
+                    <img id="etoileNotation4" src="/html/assets/imgs/notes/star_full.svg" alt="Etoile">
+                    <img id="etoileNotation5" src="/html/assets/imgs/notes/star_empty.svg" alt="Etoile">
                 </div>
+                <!--
                 <div id="divPrix">
                     <span id="prix">65</span>
                     <p>€ par nuit</p>
                 </div>
+                -->
             </article>
-            <article >
+            <article id=carateristiques>
+                <div class="barre"></div>
                 <h2>Caratéristiques du logement</h2>
                 <div id=divNbPersonnesMax>
                     <p>Nombre de personnes maximum :</p>
@@ -36,43 +40,62 @@
                 </div>
                 <div id="literie">
                     <div>
-                        <p>Chambres</p>
+                        <p>Chambres : </p>
                         <span id="nbChambres">NB</span>
                     </div>
                     <div>
-                        <p>Lits simples</p>
-                        <span id="nbLitsSimples">NB</span>
+                        <p>Lits doubles : </p>
+                        <span id="nbLitsDoubles">NB</span>
                     </div>
                     <div>
-                        <p>Lits doubles</p>
-                        <span id="nbLitsDoubles">NB</span>
+                        <p>Lits simples : </p>
+                        <span id="nbLitsSimples">NB</span>
                     </div>
                 </div>
             
             </article>
             <article>
+                <div class="barre"></div>
                 <h2>Description</h2>
                 <p id="descDet">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.</p>
             </article>
-            <!--
-            <article id=listeActivite>
-                <div>Test1</div>
-                <div>Test2</div>
+            
+            <article>
+                <div class="barre"></div>
+                <h2>Aménagements</h2>
+                <div id=listeAmenagements>
+                    <div class=badgeAmenagement>
+                        <img src="/html/assets/imgs/mobile/Sapin.svg" alt="Image aménagement 1">
+                        <h3>Jardin</p>
+                    </div>
+                    <div class=badgeAmenagement>
+                        <img src="/html/assets/imgs/mobile/Parasol.svg" alt="Image aménagement 2">
+                        <h3>Terasse</p>
+                    </div>
+                </div>
+                
             </article>
             <article>
-                <div class="activite">
-                    <img src="" alt="Image activite">
-                    <h3>Nom de l'aménagement</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.</p>
+            <div class="barre"></div>
+                <h2>Activités à proximité</h2>
+                <!--
+                <div id=listeActivite>
+                    <div class="activite">
+                        <img src="" alt="Image activite">
+                        <h3>Nom de l'activite</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.</p>
+                    </div>
+                    <div class="activite">
+                        <img src="" alt="Image activite">
+                        <h3>Nom de l'activite</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.</p>
+                    </div>
                 </div>
-                <div class="activite">
-                    <img src="" alt="Image activite">
-                    <h3>Nom de l'aménagement</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.</p>
-                </div>
+                -->
             </article>
-            -->
+            
         </section>
-    </main>
+        <div id=buffer></div>
+    </div>
 </body>
 </html>
