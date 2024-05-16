f17d6802e63f59e59fe081ab28baba75ca4be129
Lucas <lucasgourmelon12@gmail.com>
2024-05-16 10:54:15 +0200

    UPDATE ajout routes tests

--- html/index.php
+++ html/index.php
@@ -2,22 +2,39 @@
 
 $requestUrl = $_SERVER['REQUEST_URI'];
 
-// if (strpos($requestUrl, '.php') !== false) {
-//     http_response_code(403); // Interdit
-//     echo "Accès interdit";
-//     exit;
-// }
-
 $requestUrl = substr($requestUrl, 5);
+
+$routes = [
+    '/' => 'indexAction',
+    '/logement/edit' => 'editAction',
+    '/header' => 'headerAction'
+];
+
+// if (array_key_exists($requestUrl, $routes)) {
+
+//     appelFunction($routes[$requestUrl]);
+// } else {
+//     http_response_code(404);
+//     echo "Erreur 404 - Page non trouvée";
+// }
 
 switch($requestUrl) {
     case '/':
     case '':
+        // include 'Views/Front/logement/index.php';
         include './Controllers/Front/LogementController.php';
         appelFunction('indexAction');
         
         break;
     case '/logement/edit':
+        
+        break;
+    case '/header':
+        include './Views/Front/header.php';
+        break;
+    case '/api/getLogements':
+        include './Controllers/Front/Api/LogementApiController.php';
+        echo json_encode(appelFunction('indexAction')); 
         
         break;
     default:
