ca3607eeb54a27a8cc2fbe6eaf6b3e3e3bf7cbc8
Lucas <lucasgourmelon12@gmail.com>
2024-05-17 20:18:14 +0200

    UPDATE use class logement

--- html/Controllers/Front/LogementController.php
+++ html/Controllers/Front/LogementController.php
@@ -3,19 +3,51 @@
 namespace Controllers\Front;
 
 include_once 'Service/Database.php';
+include_once 'Models/Logement.php';
 
 use Service\Database;
+use Models\Logement;
 
 class LogementController {
         
     public function getAllLogements() {
-            
         $db = new Database();
-        $logements = $db->executeQuery('SELECT * FROM logement');
-            
+        $result = $db->executeQuery('SELECT * FROM logement');
+
+        $logements = [];
+        if ($result !== false) {
+            foreach ($result as $row) {
+                $logements[] = new Logement(
+                    $row['id_logement'], 
+                    $row['titre'], 
+                    $row['accroche'], 
+                    $row['personnes_max'], 
+                    $row['image_principale'], 
+                    $row['description'], 
+                    $row['latitude'], 
+                    $row['longitude'], 
+                    $row['surface_hab'], 
+                    $row['nb_chambres'], 
+                    $row['nb_lits_simples'], 
+                    $row['nb_lits_doubles'], 
+                    $row['prix_nuit_ht'], 
+                    $row['prix_nuit_ttc'], 
+                    $row['statut_propriete'], 
+                    $row['duree_min_location'], 
+                    $row['avance_resa_min'], 
+                    $row['delai_annul_max'], 
+                    $row['L_id_adresse'], 
+                    $row['L_id_compte'], 
+                    $row['L_id_type'], 
+                    $row['L_id_categorie']
+                );
+            }
+        }
+
         header('Content-Type: application/json');
-            
-        echo json_encode($logements);
+        echo json_encode(array_map(function($logement) {
+            return $logement->toArray();
+        }, $logements));
     }
         
     public function getLogementById($id) {
