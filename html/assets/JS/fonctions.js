d29de71f0c097ba91b4b72a4a5bee384de5b74b9
Lucas <lucasgourmelon12@gmail.com>
2024-05-18 18:11:22 +0200

    ADD file funtion.js with function to display stars

---
+++ html/assets/JS/fonctions.js
@@ -0,0 +1,19 @@
+
+var starFull = '<img class="noteEtoile" src="/assets/imgs/notes/star_full.svg" alt="">';
+var starEmpty = '<img class="noteEtoile" src="/assets/imgs/notes/star_empty.svg" alt="">';
+var starHalf = '<img class="noteEtoile" src="/assets/imgs/notes/star_half.svg" alt="">';
+
+export function displayNoteEtoiles(div,note) {
+    div.innerHTML = '';
+    for (let i = 0; i < 5; i++) {
+        if (note >= 1) {
+            div.innerHTML += starFull;
+            note--;
+        } else if (note >= 0.5) {
+            div.innerHTML += starHalf;
+            note = 0;
+        } else {
+            div.innerHTML += starEmpty;
+        }
+    }
+}
