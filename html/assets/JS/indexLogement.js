7208f4e46dbac3551ee897711becccdd73b1fb86
Lucas <lucasgourmelon12@gmail.com>
2024-05-19 13:57:44 +0200

    UPDATE indexLogement.js

--- html/assets/JS/indexLogement.js
+++ html/assets/JS/indexLogement.js
@@ -1,5 +1,6 @@
 // import des fonctions JS
 import { displayNoteEtoiles } from './fonctions.js';
+import { fileExists } from './fonctions.js';
 
 var header = document.getElementById('headerAccueilMobile');
 
@@ -19,27 +20,52 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch('/api/getLogementsDataForCards')
             .then(response => response.json())
             .then(data => {
-                data.forEach(logement => {
-                    let cardContent = cardTemplate.content;
+                // Test tableau vide
+                // data = [];
                 
-                    let divCard = document.createElement('div');
-                    divCard.classList.add('card');
+                if(data.length == 0){
+                    cardsContainer.innerHTML = '<h2 class="error">Aucun logement trouvé ಥ_ಥ</h2>';
+                }else{
+                    data.sort(() => Math.random() - 0.5);
+                    data.forEach(logement => {
+                        let cardContent = cardTemplate.content;
+                        
+                        let divCard = document.createElement('div');
+                        divCard.classList.add('card');
+                        
+                        divCard.setAttribute('id', logement.id_logement);
+                        
+                        divCard.appendChild(cardContent.cloneNode(true));
+                        
+                        if (logement.nom_ville && logement.titre && logement.prix_nuit_ttc && logement.description) {
+                            divCard.querySelector('.nomVille').textContent = logement.nom_ville.toUpperCase();
+                            divCard.querySelector('.titre').textContent = logement.titre;
+                            divCard.querySelector('.tarif').textContent = logement.prix_nuit_ttc + '€/nuit';
+                            divCard.querySelector('.description').textContent = logement.description;
+                            divCard.querySelector('.nbAvis').textContent = logement.nb_avis + ' avis';
+                            displayNoteEtoiles(divCard.querySelector('.note'), logement.moyenne_logement);
+                            let pathImage = logement.image_principale + '.svg';
+                            
+                            fileExists(pathImage).then(exists => {
+                                if (exists) {
+                                    divCard.querySelector('.imagePrincipale').src = pathImage;
+                                } else {
+                                    divCard.querySelector('.imagePrincipale').src = '/assets/imgs/error.png';
+                                }
+                            });
 
-                    divCard.setAttribute('id', logement.id_logement);
-                    
-                    divCard.appendChild(cardContent.cloneNode(true));
-                    
-                    divCard.querySelector('.nomVille').textContent = logement.nom_ville.toUpperCase();
-                    divCard.querySelector('.titre').textContent = logement.titre;
-                    divCard.querySelector('.tarif').textContent = logement.prix_nuit_ttc + '€/nuit';
-                    divCard.querySelector('.description').textContent = logement.description;
+                            divCard.addEventListener('click', function() {
+                                window.location.href = `/logement/${logement.id_logement}`;
+                            });
+                        } else {
+                            divCard.querySelector('.description').textContent = "Erreur lors du chargement des données du logement";
+                            divCard.querySelector('.imagePrincipale').src = '/assets/imgs/error.png';
+                        }
+                        
+                        cardsContainer.appendChild(divCard);
+                    });  
+                }
                 
-                    displayNoteEtoiles(divCard.querySelector('.note'), logement.moyenne_logement);
-
-                    divCard.querySelector('.nbAvis').textContent = logement.nb_avis + ' avis';
-                
-                    cardsContainer.appendChild(divCard);
-                });  
                 
             });
     }
