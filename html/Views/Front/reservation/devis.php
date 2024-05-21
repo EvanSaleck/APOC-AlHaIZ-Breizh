f57d349ebc798ca611f4820fa2cac3537ec5f13c
Yoh2929 <yohannmear29@gmail.com>
2024-05-21 18:17:11 +0200

    [UPDATE] Début de correction responsive desktop

--- html/Views/Front/reservation/devis.php
+++ html/Views/Front/reservation/devis.php
@@ -23,6 +23,8 @@
           
 </head>
 <body>
+    <button class="boutonPrecedent">Précédent</button>
+        
     <div class="devis">
         <div class="headerDevis">
             <button class="retour">
@@ -30,61 +32,77 @@
             </button>
             <h1 class="titreRecapDevis"> Récapitulatif de la réservation </h1>
         </div>
-        <div class="infosLogement">
-            <div class="logementResa">
-                <h2 class="titreLogDevis"> Maison de campagne LANDERNEAU </h2>
-                <div class="noteResa">
-                    <p> Note </p>
-                    <div class="note">
-                        <img class="noteEtoile" src="/assets/imgs/notes/star_full.svg" alt="">
-                        <img class="noteEtoile" src="/assets/imgs/notes/star_full.svg" alt="">
-                        <img class="noteEtoile" src="/assets/imgs/notes/star_full.svg" alt="">
-                        <img class="noteEtoile" src="/assets/imgs/notes/star_full.svg" alt="">
-                        <img class="noteEtoile" src="/assets/imgs/notes/star_empty.svg" alt="">
+        <div class="infosLogementsTOTAL">
+            <div class="infosLogementResa">
+                <div class="infosLogement">
+                    <div class="logementResa">
+                        <h2 class="titreLogDevis"> Maison de campagne LANDERNEAU </h2>
+                        <div class="noteResa">
+                            <p> Note </p>
+                            <div class="note">
+                                <img class="noteEtoile" src="/assets/imgs/notes/star_full.svg" alt="">
+                                <img class="noteEtoile" src="/assets/imgs/notes/star_full.svg" alt="">
+                                <img class="noteEtoile" src="/assets/imgs/notes/star_full.svg" alt="">
+                                <img class="noteEtoile" src="/assets/imgs/notes/star_full.svg" alt="">
+                                <img class="noteEtoile" src="/assets/imgs/notes/star_empty.svg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <figure>
+                        <img class="photoLogResa" src="/assets/imgs/offres/MaisonLanderneau.png" alt="Maison landerneau"> 
+                    </figure>
+                </div>
+
+                <div class="premiereBarre"></div>
+
+                <div class="infosReservationDev">
+                    <h3> Votre réservation </h3>
+                    <div class="info-row">
+                        <p class="info-label">Date d'arrivée :</p>
+                        <p id="date_arrivee">30/02/2025</p>
+                    </div>
+                    <div class="info-row">
+                        <p class="info-label">Date de départ :</p>
+                        <p class="date_depart">31/02/2025</p>
+                    </div>
+                    <div class="info-row">
+                        <p class="info-label">Nombre d'occupants :</p>
+                        <p class="nb_occupant">3 occupants</p>
+                    </div>
+                    <div class="deuxiemeBarre"></div>
+                    <div class="info-row">
+                        <p class="info-label">Tarif nuit :</p>
+                        <p class="prix_nuit_ttc">210€</p>
+                    </div>
+                    <div class="info-row">
+                        <p class="info-label">Taxes :</p>
+                        <p class="taxe_sejour">25€</p>
+                    </div>
+                    <div class="info-row">
+                        <p class="info-label">Total pour 12 nuits (en €) :</p>
+                        <p class="total_tarif_ttc">2560€</p>
                     </div>
                 </div>
             </div>
-            <figure>
-                <img class="photoLogResa" src="/assets/imgs/offres/MaisonLanderneau.png" alt="Maison landerneau"> 
-            </figure>
         </div>
-
-        <div class="premiereBarre"></div>
-
-        <div class="infosReservationDev">
-            <div class="info-row">
-                <p class="info-label">Date d'arrivée :</p>
-                <p id="date_arrivee">30/02/2025</p>
-            </div>
-            <div class="info-row">
-                <p class="info-label">Date de départ :</p>
-                <p class="date_depart">31/02/2025</p>
-            </div>
-            <div class="info-row">
-                <p class="info-label">Nombre d'occupants :</p>
-                <p class="nb_occupant">3 occupants</p>
-            </div>
-            <div class="deuxiemeBarre"></div>
-            <div class="info-row">
-                <p class="info-label">Tarif nuit :</p>
-                <p class="prix_nuit_ttc">210€</p>
-            </div>
-            <div class="info-row">
-                <p class="info-label">Taxes :</p>
-                <p class="taxe_sejour">25€</p>
-            </div>
-            <div class="info-row">
-                <p class="info-label">Total pour 12 nuits (en €) :</p>
-                <p class="total_tarif_ttc">2560€</p>
-            </div>
-        </div>
-
+        
         <div class="PaiementResa">
             <p> Payer via </p>
             <div class="ChoixPaiement">
                 <div class="PaiementVisa">
                     <img class="imageVisa" src="/assets/imgs/paiement/visa.png" alt="Image VISA">
                     <p> Carte bancaire </p>
+                    <div class="styleInput">
+                        <input id="first-name" minlength="9" maxlength="9" placeholder="Carte de crédit ou débit" />
+                    </div>
+                    <div class="styleInput">
+                        <input id="first-name" minlength="9" maxlength="9" placeholder="Expiration" />
+                        <input id="first-name" minlength="9" maxlength="9" placeholder="Cryptogramme" />
+                    </div>
+                    <div class="styleInput">
+                        <input id="first-name" minlength="9" maxlength="9" placeholder="Code postal" />
+                        <input id="first-name" minlength="9" maxlength="9" placeholder="Pays" />
+                    </div>
                 </div>
                 <div class="PaiementPaypal">
                     <img class="imagePaypal" src="/assets/imgs/paiement/paypal.png" alt="Image Paypal">
