7c52a1e104fbfd6b06d38c8e78e6a1e5bf99e8cc
CouchWhisp <charlesalexandre.gautier@gmail.com>
2024-06-26 08:36:27 +0200

    Repris le index.php du develop

--- html/index.php
+++ html/index.php
@@ -51,31 +51,20 @@ switch($requestUrl) {
         $abonnementICalController->getAbonnementsICalByProprietaire($prop);
         break;
     
-    // routes gildas
-    case '/export/ical':
-    case '/export/ical/':
-        // $reservationController->exportIcal();
-        include_once './Views/Back/reservation/exportICal.php';
-        break;
-    
-    case '/service/exportICal':
-    case '/service/exportICal/':
-        $reservationController->exportIcal();
-        break;
-
     case '/api/reservations/abonnements/iCal/new':
     case '/api/reservations/abonnement/iCal/new/':
-        $abonnementICalController->newAction();
+        $abonnementICalController->newAction();        
         break;
 
-    case preg_match('/^\/api\/reservations\/abonnements\/iCal\/edit\/\d+$/', $requestUrl) ? true : false:
+    case preg_match('/^\/api\/reservations\/abonnements\/iCal\/edit\/\d+$/', $requestUrl) ? true : false:   
     case preg_match('/^\/api\/reservations\/abonnements\/iCal\/edit\/\d+\/$/', $requestUrl) ? true : false:
         $url_parts = explode('/', $requestUrl);
         $id = end($url_parts);
+        
         $abonnementICalController->editAction($id);
         break;
 
-
+    
     // on créé la route de suppression : /api/reservations/abonnements/iCal/delete/{id}
     // ainsi que  : /api/reservations/abonnements/iCal/delete/{id}/
     case preg_match('/^\/api\/reservations\/abonnements\/iCal\/delete\/\d+$/', $requestUrl) ? true : false:   
@@ -84,196 +73,110 @@ switch($requestUrl) {
         $id = end($url_parts);
         $abonnementICalController->deleteAction($id);
         break;
-
+    
     case preg_match('/^\/reservations\/abonnements\/iCal\/edit\/\d+$/', $requestUrl) ? true : false:   
     case preg_match('/^\/reservations\/abonnements\/iCal\/edit\/\d+\/$/', $requestUrl) ? true : false:
         include_once './Views/Back/reservation/abonnementICal.php';
         break;
-        
+
     case preg_match('/^\/api\/reservations\/abonnements\/iCal\/getDataICal\/\d+$/', $requestUrl) ? true : false:
     case preg_match('/^\/api\/reservations\/abonnements\/iCal\/getDataICal\/\d+\/$/', $requestUrl) ? true : false:
         $url_parts = explode('/', $requestUrl);
         $id = end($url_parts);
         $abonnementICalController->getDataICal($id);
         break;
-    
 
-    //////////////////////////////
-    //                          //
-    // ROUTES VUES FRONT OFFICE //////////////////////////////////////////////////////////////////////////////////////
-    //                          //
-    //////////////////////////////
-
-    // Page de détails du compte d'un client
-    case '/compte':
-    case '/compte/':
-        include_once './Views/Front/compte/detailsCompte.php';
-        break;
-
-    // Page principale Front
+    // Routes des vues front office 
     case '/':
     case '':
         include_once './Views/Front/logement/indexLogement.php';
         break;
         
-    // Page de détails d'un logement pour le client
     case '/logement':
     case '/logement/':
         include_once './Views/Front/logement/detailsLogement.php';
         break;
-    
-    case '/detailReservation':
-    case '/detailReservation/':
-        if(isset($_SESSION['client'])) {
-            include './Views/Front/reservation/DetailReservation.php';
-        }
-        else { header('Location: /'); }
+    case '/compte':
+    case '/compte/':
+        include_once './Views/Front/compte/detailsCompte.php';
         break;
-
+    case '/connexion':
+    case '/connexion/':
+        include_once './Views/Front/compte/connexionCompte.php';
+        break;
     case '/reservation/devis':
     case '/reservation/devis/':
-        if(isset($_SESSION['client'])) {
+        if (!isset($_SESSION['client'])) {
+            header('Location: /');
+        } else {
             include_once './Views/Front/reservation/devis.php';
         }
-        else { header('Location: /'); }
+
         break;
 
+    
+    case preg_match('/^\/facture\/\d+$/', $requestUrl) ? true : false:
+        $url_parts = explode('/', $requestUrl);
 
-
-    /////////////////////////////
-    //                         //
-    // ROUTES VUES BACK OFFICE ///////////////////////////////////////////////////////////////////////////////////////
-    //                         //
-    /////////////////////////////
-
-    // Page de détails du compte d'un propriétaire
-    case '/back/detailsCompte':
-    case '/back/detailsCompte/':
-        include './Views/Back/compte/detailsCompte.php';
+        include_once './Views/facture.php';
+        
         break;
 
-    // Page principale Back / Page de liste des réservations du propriétaire
+    case '/test':
+        echo '<script>window.open("/facture/1", "_blank")</script>';
+        break;
+
+    
     case '/Back/reservations':
     case '/Back/reservations/':
-        if(isset($_SESSION['proprio'])) {
+        include_once('Views/Back/reservation/listeReservations.php');
+        if(!isset($_SESSION['proprio'])) {
             include_once('Views/Back/reservation/listeReservations.php');
+            
+        }else {
+            header('Location: /connexionProprietaire');
         }
-        else { header('Location: /connexionProprietaire'); }
-        break;
-     
-    // Page de détails d'une réservation pour le propriétaire
-    case '/Back/reservations/details':
-    case '/Back/reservations/details/':
-        if(isset($_SESSION['proprio'])) {
-            include_once('Views/Back/reservation/detailsReservation.php');
-        }
-        else { header('Location: /connexionProprietaire'); }
         break;
 
-    // Page de liste des logements
     case '/logements':
     case '/logements/':
         if(isset($_SESSION['proprio'])) {
             include_once('Views/Back/logement/listeLogements.php');
+        }else {
+            header('Location: /connexionProprietaire');
         }
-        else { header('Location: /connexionProprietaire'); }
         break;
 
-    case '/logements':
-    case '/logements/':
-        include './Views/Back/logement/listeLogements.php';
+    // routes du back
+    case '/logement/new':
+    case '/logement/new/':
+         if(isset($_SESSION['proprio'])) {
+            include './Views/Back/logement/newLogement.php';
+        }else {
+            header('Location: /connexionProprietaire');
+        }
         break;
 
+    case '/logements/details':
+        case '/logements/details/';
+            include './Views/Back/logement/detailsLogement.php';
+            break;
+        
+    case '/logements/details/modifier':
+        case '/logements/details/modifier';
+            include './Views/Back/logement/modifierLogement.php';
+            break;
+    
+    // routes back office
     case '/logement':
     case '/logement/':
         include './Views/Back/logement/listeLogements.php';
         break;
 
-    // Page de détails d'un logement
-    case '/logements/details':
-    case '/logements/details/';
-        include './Views/Back/logement/detailsLogement.php';
-        break;
-
-    // Page de création d'un logement
-    case '/logement/new':
-    case '/logement/new/':
-        if(isset($_SESSION['proprio'])) {
-            include './Views/Back/logement/newLogement.php';
-        }
-        else { header('Location: /connexionProprietaire'); }
-        break;
-     
-    // Page de modification d'un logement
-    case '/logements/details/modifier':
-    case '/logements/details/modifier';
-        include './Views/Back/logement/modifierLogement.php';
-        break;
-
-
-
-    ///////////////////////
-    //                   //
-    // ROUTES POUR L'API /////////////////////////////////////////////////////////////////////////////////////////////
-    //                   //
-    ///////////////////////
-
-    // Fonctions de connexion / inscription pour les clients
-    case '/api/ConnexionClient':
-    case 'api/ConnexionClient':
-        $data = $_POST;
-        $utilisateurController->connexionClient($data);
-        break;
-            
-    case 'api/InscriptionClient':
-    case '/api/InscriptionClient':
-        $data = $_POST;
-        $utilisateurController->inscriptionClient($data);
-        break;
-    
-    // Fonction de déconnection pour les clients, renvoie sur la page principale Front
-    case '/Deconnexion':
-    case '/Deconnexion/':
-        $_SESSION = array();
-        session_destroy();
-        header('Location: /');
-        break;
-
-    // Page de connexion pour les propriétaires
     case '/connexionProprietaire':
     case '/connexionProprietaire/':
         include './Views/Back/connexionProprietaire.php';
         break;
-    
-    // Fonctions de connexion / inscription pour les propriétaires
-    case '/api/ConnexionProprio':
-    case 'api/ConnexionProprio':
-        $data = $_POST;
-        $utilisateurController->connexionProprio($data);
-        break;
-
-    case 'api/InscriptionProprio':
-    case '/api/InscriptionProprio':
-        $data = $_POST;
-        $utilisateurController->inscriptionProprio($data);
-        break;
-
-    // Fonction de déconnexion pour les propriétaires, renvoie sur la page de connexion Back
-    case '/DeconnexionProprio':
-    case '/DeconnexionProprio/':
-        $_SESSION = array();
-        session_destroy();
-        header('Location: /connexionProprietaire');
-        break;
-
-    
-    
-    // FONCTIONS D'API POUR LES LOGEMENTS
-    case '/api/getLogements':
-        header('Content-Type: application/json');
-        echo $logementController->getAllLogements();
-        break;
-
     case '/api/getLogementsDataForCards':
         header('Content-Type: application/json');
         echo $logementController->getLogementsDataForCards();
@@ -289,88 +192,119 @@ switch($requestUrl) {
         $logementController->processFormUpdateLogement();
         break;
     
+    case '/api/getReservations':
+    case 'api/getReservations':
+        //$reservationController->getAllReservation();
+        $idProp = 7;
+        $reservationController->getReservationByOwnerId($prop);
+        break;
+    case '/api/getReservationById':
+    case 'api/getReservationById':
+        $data = $_POST;
+        $reservationController->getReservationById($data);
+        break;
+        
     case 'api/getTypeOfLogementById/':
     case 'api/getTypeOfLogementById/':
         $data = $_POST;
         $logementController->getTypeOfLogementById($data);
         break;
-
     /*
     case 'api/getCategorieOfLogementById/':
     case 'api/getCategorieOfLogementById/':
         $data = $_POST;
         $logementController->getCategorieOfLogementById($data);
         break;
-    */
-        
-    case '/api/getLogementById':
-    case 'api/getLogementById':
-        $data = $_POST;
-        $logementController->getLogementById($data['id']);
-        break;
-                
-    case '/api/getLogementsByProprietaireId':
-    case '/api/getLogementsByProprietaireId/':
-        // $data = $_POST;
-        $logementController->getLogementsByProprietaireId($prop);
-        break;
-
-
-
-    // FONCTIONS D'API POUR LES RESERVATIONS
-    case '/api/getReservationsClient':
-    case 'api/getReservationsClient':
-        $data = $_POST;
-        $reservationController->getReservationByClientId($data['id']);
-        break;
-
-    case '/api/getReservationsProprietaire':
-    case 'api/getReservationsProprietaire':
-        $data = $_POST;
-        $reservationController->getReservationByOwnerId($data['id']);
-        break;
-
-    // PLUS UTILISE, à supprimer pendant le merge final du Index.php
-    case '/api/getReservations':
-    case 'api/getReservations':
-        $idProp = 7;
-        $reservationController->getReservationByOwnerId($prop);
-        break;
-
-    case '/api/getReservationById':
-    case 'api/getReservationById':
-        $data = $_POST;
-        $reservationController->getReservationById($data);
-        break;
-
-
-
-    case '/api/getClientById':
-    case 'api/getClientById':
-        $data = $_POST;
-        $utilisateurController->getCompteClientDetails($data['id']);
-        break;
+        */
 
     case '/api/getProprioById':
     case 'api/getProprioById':
         $data = $_POST;
         $utilisateurController->getProprioById($data['id']);
         break;
+    case '/api/getLogementById':
+    case 'api/getLogementById':
+        $data = $_POST;
+        $logementController->getLogementById($data['id']);
+        break;
+
+    case '/api/getLogementsByProprietaireId':
+    case '/api/getLogementsByProprietaireId/':
+        // $data = $_POST;
+        $logementController->getLogementsByProprietaireId($prop);
+        break;
+
+    // Routes des API
+    case '/Deconnexion':
+    case '/Deconnexion/':
+        $_SESSION = array();
+        session_destroy();
+        header('Location: /');
+        break;
     
+    case '/DeconnexionProprio':
+        case '/DeconnexionProprio/':
+            $_SESSION = array();
+            session_destroy();
+            header('Location: /connexionProprietaire');
+            break;
+
+    case '/back/detailsCompte':
+        case '/back/detailsCompte/':
+            include './Views/Back/compte/detailsCompte.php';
+            break;
+    case '/gestionTokens':
+        case '/gestionTokens/':
+            include './Views/Back/compte/gestionTokens.php';
+            break;
+
+    case '/api/ConnexionClient':
+    case 'api/ConnexionClient':
+        $data = $_POST;
+
+        $utilisateurController->connexionClient($data);
+        break;
+
+    case 'api/InscriptionClient':
+    case '/api/InscriptionClient':
+        $data = $_POST;
+        $utilisateurController->inscriptionClient($data);
+        break;
+
+    case '/api/ConnexionProprio':
+    case 'api/ConnexionProprio':
+            $data = $_POST;
+            $utilisateurController->connexionProprio($data);
+        break;
+
+    case 'api/InscriptionProprio':
+    case '/api/InscriptionProprio':
+        $data = $_POST;
+        $utilisateurController->inscriptionProprio($data);
+        break;
+        /*
+    case '/api/getCategorieOfLogementById':
+    case '/api/getCategorieOfLogementById/':
+        $data = $_POST;
+        $categorieLogement->getCategorieOfLogementById($data);
+        break;
+        */
+    
+
+    case '/api/getLogements':
+        header('Content-Type: application/json');
+        echo $logementController->getAllLogements();
+        break;
+
     case '/api/getCompteClientDetails':
         // $idCompte = $_SESSION['client'];
         $client = json_decode($_SESSION['client']);
         $idCompte = $client->id_compte;
         $utilisateurController->getCompteClientDetails($idCompte);
         break;
-
-    case '/api/getCompteClientDetailsById':
-        $data = $_POST;
-        $utilisateurController->getProprioById($data['id']);
-        break;
     
     case '/api/getCompteProprioDetails':
-    case '/api/getCompteProprioDetails/':
+        case '/api/getCompteProprioDetails/':
         $proprio = json_decode($_SESSION['proprio']);
         $idCompte = $proprio->id_compte;
         $utilisateurController->getCompteProprioDetails($idCompte);
@@ -388,14 +322,15 @@ switch($requestUrl) {
         $reservationController->saveReservation($data, $idcpt);
         break;
 
-
-    // Page de gestion des tokens
-    case '/gestionTokens':
-    case '/gestionTokens/':
-        include './Views/Back/compte/gestionTokens.php';
+    case '/detailReservation':
+    case '/detailReservation/':
+        if(!isset($_SESSION['client'])) {
+            header('Location: /');
+        }else {
+            include './Views/Front/reservation/DetailReservation.php';
+        }
         break;
 
-    // Récupération de tous les tokens d'un propriétaire
     case '/api/getAllTokenById':
     case '/api/getAllTokenById/':
         $proprio = json_decode($_SESSION['proprio']);
@@ -403,14 +338,12 @@ switch($requestUrl) {
         $utilisateurController->getAllTokenById($id);
         break;
 
-    // Suppression d'un token d'un propriétaire
     case '/api/deleteToken/':
         case '/api/deleteToken':
         $data = $_POST;
         $utilisateurController->deleteToken($data);
         break;
 
-    // Création d'un token pour un propriétaire
     case '/api/generateToken':
     case '/api/generateToken/':
         $data = $_POST;
@@ -459,17 +392,6 @@ switch($requestUrl) {
         echo $reservationController->getDataReservationById($idResa);
         break;
     */
-
-    case preg_match('/^\/facture\/\d+$/', $requestUrl) ? true : false:
-        $url_parts = explode('/', $requestUrl);
-    
-        include_once './Views/facture.php';
-            
-        break;
-    
-    case '/test':
-        echo '<script>window.open("/facture/1", "_blank")</script>';
-        break;
 
     default:
         http_response_code(404);
